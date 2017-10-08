2154ec0213958fa0ae5ef0fac5f7a9c548f3aed6
cubaeurokatmic <cubaeurokatmic@gmail.com>
2017-10-08 12:05:03 +0200

    overscan test

--- lib/python/Screens/StartWizard.py
+++ lib/python/Screens/StartWizard.py
@@ -3,17 +3,21 @@ from Screens.WizardLanguage import WizardLanguage
 from Screens.VideoWizard import VideoWizard
 from Screens.Rc import Rc
 from Screens.Screen import Screen
-
 from boxbranding import getBoxType
+try:
+	from Plugins.SystemPlugins.OSDPositionSetup.overscanwizard import OverscanWizard
+except:
+	OverscanWizard = None
 
 from Components.Pixmap import Pixmap
 from Components.config import config, ConfigBoolean, configfile
-
+from Components.SystemInfo import SystemInfo
 from LanguageSelection import LanguageWizard
 
 config.misc.firstrun = ConfigBoolean(default = True)
 config.misc.languageselected = ConfigBoolean(default = True)
 config.misc.videowizardenabled = ConfigBoolean(default = True)
+config.misc.do_overscanwizard = ConfigBoolean(default = OverscanWizard and config.skin.primary_skin.value == "SimpleLD/skin.xml" or config.skin.primary_skin.value == "MetrixHD/skin.xml")
 
 class StartWizard(WizardLanguage, Rc):
 	def __init__(self, session, silent = True, showSteps = False, neededTag = None):
@@ -21,8 +25,6 @@ class StartWizard(WizardLanguage, Rc):
 		WizardLanguage.__init__(self, session, showSteps = False)
 		Rc.__init__(self)
 		self["wizard"] = Pixmap()
-		self["HelpWindow"] = Pixmap()
-		self["HelpWindow"].hide()
 		#Screen.setTitle(self, _("Welcome..."))
 		Screen.setTitle(self, _("StartWizard"))
 
@@ -38,12 +40,10 @@ class StartWizard(WizardLanguage, Rc):
 		config.misc.firstrun.save()
 		configfile.save()
 
-
-# mytest.py#L528ff - RestoreSettings
 wizardManager.registerWizard(VideoWizard, config.misc.videowizardenabled.value, priority = 0)
 wizardManager.registerWizard(LanguageWizard, config.misc.languageselected.value, priority = 2)
-# FrontprocessorUpgrade FPUpgrade priority = 8
-# FrontprocessorUpgrade SystemMessage priority = 9
+if OverscanWizard is not None:
+	wizardManager.registerWizard(OverscanWizard, config.misc.do_overscanwizard.value, priority = 10)
 wizardManager.registerWizard(StartWizard, config.misc.firstrun.value, priority = 20)
 # StartWizard calls InstallWizard
 # NetworkWizard priority = 25
